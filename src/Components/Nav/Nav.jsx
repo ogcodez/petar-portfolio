dc8354850642187e5b9758d25e94d7bcd6ece7ea
ogcodez <trcognjen@gmail.com>
2023-04-21 01:32:18 +0200

    second commit

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -37,22 +37,7 @@ const Nav = () => {
                 </li>
             </ul >
             <div className={styles["navbar-responsive-menu"]}>
-                <Popup
-                    trigger={<HambergerMenu size="32" color="var(--white-100)" />}
-                    position="right top"
-                    on="hover"
-                    closeOnDocumentClick
-                    mouseLeaveDelay={300}
-                    mouseEnterDelay={0}
-                    contentStyle={{ padding: '0px', border: 'none' }}
-                    arrow={false}
-                >
-                    <div className="menu">
-                        <div className="menu-item"> item 1</div>
-                        <div className="menu-item"> item 2</div>
-                        <div className="menu-item"> item 3</div>
-                    </div>
-                </Popup>
+                <HambergerMenu size="32" color="var(--white-100)" />
             </div>
         </nav >
     )
